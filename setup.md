49b2a95797c41892f7ed854b09d1e46d118459be
Gilbert Ardila <gilbertferney@gmail.com>
2023-04-08 11:33:10 -0500

    validando los datos de creación y actualización de producto

--- setup.md
+++ setup.md
@@ -3,3 +3,15 @@
 - npm run start:dev
 - nest g co controllers/categories --flat para crear controladores
 - nest g s services/products --flat para crear servicios
+
+## adding new dependency
+
+- npm i class-validator class-transformer
+- npm i @nestjs/mapped-types
+
+### to be aware
+
+- para activar las validaciones vamos al archivo main.ts y
+  hacemos la siguiente importación: import { ValidationPipe } from '@nestjs/common';
+  luego activamos el pipe de forma global con la siguiente linea: app.useGlobalPipes(new ValidationPipe());
+  antes de poner en eschucha el puerto
--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,15 +1,34 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUrl,
+  Max,
+  Min,
+} from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
   readonly name: string;
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
   readonly price: number;
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
   readonly stock: number;
+  @IsNotEmpty()
+  @IsUrl()
   readonly image: string;
 }
 
-export class UpdateProductDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+//utilizamos las mismas validaciones pero cada argumento va a ser opcional
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
